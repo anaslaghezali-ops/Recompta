3e70abe2cacdf25b4bd3c51b8304e01e99296fd1
anaslaghezali-ops <anaslaghezali@gmail.com>
2026-08-13 20:17:32 +0100

    Update supabase-config.js

--- docs/supabase-config.js
+++ docs/supabase-config.js
@@ -6,4 +6,4 @@ export const SUPABASE_URL = "https://pbyoxfxngfutoiqjirkx.supabase.co";
  * Dashboard → Project Settings → API Keys
  * Ne mettez JAMAIS la clé service_role ici.
  */
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhzb2pmaHRhYm1mY3pocGl3dXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2MzM1NjcsImV4cCI6MjEwMjIwOTU2N30.cAg84siHXy5rBLibWEZYlObCRdO_pAzl19qYhnLM2MA";
